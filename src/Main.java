3d321285321012c04ee2f6136aa7cba2be956e97
Avi Rahimov <rahimovavi3@gmail.com>
2022-11-08 19:37:10 +0200

    update the class

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        UndoableStringBuilder usb = new UndoableStringBuilder("");
-        //		st = st.replace(3, 5, "eat");
-        //		st = st.reverse();
-        //		st = st.append("avi");
-        //		System.out.println(st);
-        //		char [] c = {'a', 'e', 'd', 'r', 'i', 'o', 'y', 't', 'b'};
-        //          { 0 ,  1 ,  2 ,  3 ,  4 ,  5 ,  6 ,  7 ,  8 }=9
-        //		st = st.delete(0, 6);
-        //		System.out.println(st.insert(0, "Asd"));
+        UndoableStringBuilder usb = new UndoableStringBuilder();
+        usb.append("to be or not to be") ;
         System.out.println(usb) ;
-        usb.append("to be  or not to be") ;
+        usb.replace(3, 5, "eat") ;
         System.out.println(usb) ;
-        usb.replace(3, 6, "eat") ;
-        System.out.println(usb) ;
-        usb.replace(17, 20, "eat") ;
+        usb.replace(17, 19, "eat") ;
         System.out.println(usb) ;
         usb.reverse();
-        usb.append(null);
+        System.out.println(usb) ;
+        System.out.println();
+        usb.undo();
+        System.out.println(usb) ;
+        usb.undo();
+        System.out.println(usb) ;
+        usb.undo();
         System.out.println(usb);
-        //			System.out.println(usb) ;
-        //			System.out.println();
-        //			usb.undo();
-        //			System.out.println(usb) ;
-        //			usb.undo();
-        //			System.out.println(usb) ;
-        //			usb.undo();
-        //			System.out.println(usb) ;
         //			usb.undo();
         //			usb.undo();
 //        			usb.undo();
-        			}
+    }
 }
--- src/UndoableStringBuilder.java
+++ src/UndoableStringBuilder.java
@@ -5,21 +5,11 @@ import java.util.*;
  * @version 01/11/22
  */
 public class UndoableStringBuilder {
-    private UndoableStringBuilder s = new UndoableStringBuilder();
-    private Stack<String> stk = new Stack<String>();
-    /**
-     *
-     * @param str the string we want to initialize into StringBuilder object
-     */
-    public UndoableStringBuilder(String str) {
-        (this.s).append(str);
-    }
-    /**
-     *
-     * @throws NullPointerException if we're trying to create null StringBuilder
-     */
-    public UndoableStringBuilder() throws  NullPointerException {
-        throw new NullPointerException("You can't build null string");
+    private StringBuilder s = new StringBuilder();
+    private Stack<String> stk;
+    public UndoableStringBuilder(){
+        s = new StringBuilder();
+        stk = new Stack<>();
     }
     /**
      *
@@ -27,11 +17,9 @@ public class UndoableStringBuilder {
      * @return the new StringBuilder that caused by concatenating the two strings
      */
     public UndoableStringBuilder append(String str) {
-        if(str==null)
-            throw new NullPointerException("You can't append null string");
         s = ((this.s).append(str));
         stk.push(s.toString());
-        return s;
+        return this;
     }
     /**
      *
@@ -40,9 +28,11 @@ public class UndoableStringBuilder {
      * @return a new StringBuilder after the string insertion in "offset" starting index
      */
     public UndoableStringBuilder insert(int offset, String str) {
+        if(offset>s.length() || offset<0)
+            throw new StringIndexOutOfBoundsException("Your offset index is out of the String bounds");
         s = (this.s).insert(offset, str);
         stk.push(s.toString());
-        return s;
+        return this;
     }
     /**
      *
@@ -51,9 +41,11 @@ public class UndoableStringBuilder {
      * @return a new StringBuilder after deleting the string in index start to (end-1)
      */
     public UndoableStringBuilder delete(int start, int end) {
+        if (start<0 || end<0 || start>end)
+            throw new StringIndexOutOfBoundsException("You can't start or end in negative index and the start index cannot be greater than the end index");
         s = (this.s).delete(start, end);
         stk.push(s.toString());
-        return s;
+        return this;
     }
     /**
      *
@@ -63,9 +55,13 @@ public class UndoableStringBuilder {
      * @return the full StringBuilder after the changes that created by swapping the string in the indexes that specified
      */
     public UndoableStringBuilder replace(int start,int end, String str) {
+        if(str == null)
+            throw new NullPointerException("You can't replace the string by null");
+        if(start<0 || end<0 || start>end)
+            throw new StringIndexOutOfBoundsException("You can't start or end in negative index and the start index cannot be greater than the end index");
         s = (this.s).replace(start, end, str);
         stk.push(s.toString());
-        return s;
+        return this;
     }
     /**
      *
@@ -74,20 +70,19 @@ public class UndoableStringBuilder {
     public UndoableStringBuilder reverse() {
         s = (this.s).reverse();
         stk.push(s.toString());
-        return s;
+        return this;
     }
     /**
-     *
      * @throws EmptyStackException if we're trying to access to empty stack
      */
     public void undo() throws EmptyStackException {
-        System.out.println(stk.toString());
+//        System.out.println(stk.toString());
         if(stk.isEmpty()) {
-            System.err.println("you are trying to access to empty stack");
+            System.err.println("you can't access to empty stack");
             throw new EmptyStackException();
         }
         else {
-            stk.pop();
+           s.replace(0, s.length(), stk.pop());
         }
     }
     /**
