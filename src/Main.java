12d39176db5efc1ee7789c9a7e658f2ee49d866d
Avi Rahimov <rahimovavi3@gmail.com>
2022-11-08 17:58:34 +0200

    try to add intellij to github

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        UndoableStringBuilder usb = new UndoableStringBuilder("");
+        //		st = st.replace(3, 5, "eat");
+        //		st = st.reverse();
+        //		st = st.append("avi");
+        //		System.out.println(st);
+        //		char [] c = {'a', 'e', 'd', 'r', 'i', 'o', 'y', 't', 'b'};
+        //          { 0 ,  1 ,  2 ,  3 ,  4 ,  5 ,  6 ,  7 ,  8 }=9
+        //		st = st.delete(0, 6);
+        //		System.out.println(st.insert(0, "Asd"));
+        System.out.println(usb) ;
+        usb.append("to be  or not to be") ;
+        System.out.println(usb) ;
+        usb.replace(3, 6, "eat") ;
+        System.out.println(usb) ;
+        usb.replace(17, 20, "eat") ;
+        System.out.println(usb) ;
+        usb.reverse();
+        usb.append(null);
+        System.out.println(usb);
+        //			System.out.println(usb) ;
+        //			System.out.println();
+        //			usb.undo();
+        //			System.out.println(usb) ;
+        //			usb.undo();
+        //			System.out.println(usb) ;
+        //			usb.undo();
+        //			System.out.println(usb) ;
+        //			usb.undo();
+        //			usb.undo();
+//        			usb.undo();
+        			}
 }
